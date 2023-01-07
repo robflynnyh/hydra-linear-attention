f1468c79a206211d3ac547a28ef861e0fe255cc4
Rob <rjflynn2@sheffield.ac.uk>
2023-01-07 15:36:57 +0000

    !

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # hydra-linear-attention
 Implementation of the thingy described in this paper: https://arxiv.org/pdf/2209.07484.pdf
 
-- code is mostly taken from the appendix of the paper its pretty simple
-- added some options for output layer i've found a cheap scale and bias to be fine, not clear what they do in the paper 
+- code is mostly taken from the appendix of the paper its pretty simple 
 - basically its linear attention with heads equeal to the feature dim, they use l2 norm as the kernel fn rather than softmax as it allows you to scale the "head" dimension, which makes it faster
 - idk if it's descriptive to say stuff like this is similar to regular attention - I see it being more similar to something like squeeze and excite layers
--- hydra.py
+++ hydra.py
@@ -1,18 +1,14 @@
 import torch.nn as nn, torch
 
 class HydraAttention(nn.Module):
-    def __init__(self, d_model, output_layer='scale_and_bias'):
+    def __init__(self, d_model, output_layer='linear'):
         '''
-        output_layer: 'scale_and_bias' | 'linear' | 'none'
+        output_layer: 'linear' | 'none'
         '''
         super(HydraAttention, self).__init__()
         self.d_model = d_model
         self.qkv = nn.Linear(d_model, d_model * 3)
-        if output_layer == 'scale_and_bias':
-            self.scale = nn.Parameter(torch.ones(1, 1, d_model))
-            self.bias = nn.Parameter(torch.zeros(1, 1, d_model))
-            self.out = lambda x: x * self.scale + self.bias
-        elif output_layer == 'linear':
+        if output_layer == 'linear':
             self.out = nn.Linear(d_model, d_model)
         elif output_layer == 'none':
             self.out = nn.Identity()
